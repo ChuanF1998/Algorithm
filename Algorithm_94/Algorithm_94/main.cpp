db286cdb90efcdf58edae0cd8d17fe325a7437d9
ChuanF1998 <895952166@qq.com>
2020-02-03 16:48:30 +0800

    表示数字的字符串

--- Algorithm_94/Algorithm_94/main.cpp
+++ Algorithm_94/Algorithm_94/main.cpp
@@ -1,5 +1,16 @@
+/*
+题目描述
+请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
+例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。 
+但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。
+*/
 
 
+/*
+数字串： .123 表示 0.123；12. 表示12；12.123表示12.123，意思是小数点前后只有有一边有数字都可以
+次方e或E: e前面必须要数字（小数和整数都可），e后面必须是整数，不能是小数。
+在遍历的时候，字符串要分为带符号和不带符号
+*/
 
 class Solution {
 public:
@@ -11,8 +22,7 @@ public:
 		bool isNumeric = scanUnsignedInt(string);
 		if (*string == '.') {
 			string++;
-			scanUnsignedInt(string);
-			isNumeric = isNumeric || scanUnsignedInt(string);
+			isNumeric = scanInt(string) || isNumeric;
 		}
 		if (*string == 'e' || *string == 'E') {
 			string++;
@@ -21,18 +31,24 @@ public:
 		return isNumeric && *string == '\0';
 	}
 
+	//带符号
 	bool scanUnsignedInt(char*& str)
 	{
 		if (*str == '-' || *str == '+') {
 			str++;
 		}
+		return scanInt(str);
+	}
+
+	//不带符号
+	bool scanInt(char*& str)
+	{
 		char* prev = str;
 		while (*str != '\0' && *str >= '0' && *str <= '9') {
 			str++;
 		}
 		return str > prev;
 	}
-
 };
 
 
