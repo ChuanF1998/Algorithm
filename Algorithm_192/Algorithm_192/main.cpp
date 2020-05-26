6c33e0386a110e9e0772fbd80f1b8c1f5849149d
ChuanF1998 <895952166@qq.com>
2020-05-26 23:47:29 +0800

    qwer

---
+++ Algorithm_192/Algorithm_192/main.cpp
@@ -0,0 +1,13 @@
+
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> arr[5];
+	vector<vector<int> > arr2(5);
+	vector<int> arr1(5);
+	arr[1].push_back(1);
+	arr2[1].push_back(3);
+	return 0;
+}
